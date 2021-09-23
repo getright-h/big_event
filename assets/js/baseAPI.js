300c4e618328cf48a6f4edc8f2c9b7b106d29244
zh3ang <906106552@qq.com>
2021-09-23 17:12:56 +0800

    修改了baseapi

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,4 @@
 //每次调用ajax发送请求时，jq都会调用此函数，opitons可以拿到我们提供的配置对象
 $.ajaxPrefilter(function (options) {
-    options.url = 'http://api-breakingnews-web.itheima.net' + options;
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
 })
